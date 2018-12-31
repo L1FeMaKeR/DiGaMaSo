e76f5ddac9d1fb94323124381b660b1d8ba46446
L1FeMaKeR <chbur1@gmx.de>
2018-12-31 16:33:49 +0100

    Setup the initial windows layer compilation

---
+++ .vscode/build.bat
@@ -0,0 +1,7 @@
+@echo off
+call "F:\Programme\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64     
+set compilerflags=/Od /Zi /EHsc
+set linkerflags=/OUT:DiGaMaSo.exe
+pushd ..\build
+"F:\Programme\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.14.26428\bin\Hostx64\x64\cl.exe" %compilerflags% ..\source\win32\win32.cpp /link %linkerflags%
+popd
---
+++ source/win32/win32.cpp
@@ -0,0 +1,7 @@
+#include <Windows.h>
+
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
+{
+    return 0;
+}
