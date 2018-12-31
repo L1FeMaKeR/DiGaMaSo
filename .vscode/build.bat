acea7a3157bc0f583b131d7b666d94c77d270c81
L1FeMaKeR <chbur1@gmx.de>
2018-12-31 17:16:59 +0100

    Now the Windows Build with a simple window compiles and can be build

--- .vscode/build.bat
+++ .vscode/build.bat
@@ -1,7 +1,7 @@
 @echo off
 call "F:\Programme\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64     
 set compilerflags=/Od /Zi /EHsc
-set linkerflags=/OUT:DiGaMaSo.exe
+set linkerflags=/OUT:DiGaMaSo.exe user32.lib
 pushd ..\build
 "F:\Programme\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.14.26428\bin\Hostx64\x64\cl.exe" %compilerflags% ..\source\win32\win32.cpp /link %linkerflags%
 popd
--- source/win32/win32.cpp
+++ source/win32/win32.cpp
@@ -1,7 +1,54 @@
 #include <Windows.h>
 
+static bool globalRunning = true;
 
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
+LRESULT CALLBACK WindowsCallback(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    LRESULT result = 0;
+
+    
+    switch (message)
+    {
+        case WM_CLOSE:
+        {
+            globalRunning = false;
+        }break;
+        case WM_DESTROY:
+        {
+            globalRunning = false;
+        }break;
+    
+        default:
+        {
+            result = DefWindowProcA(window, message, wParam, lParam);
+        }break;
+    }
+
+    return result;
+}
+
+int WinMain(HINSTANCE instance, HINSTANCE prevInstance, PSTR cmdLine, INT cmdShow)
+{
+    WNDCLASSA WindowClass = {};
+    WindowClass.style = CS_HREDRAW | CS_VREDRAW;
+    WindowClass.lpfnWndProc = WindowsCallback;
+    WindowClass.hInstance = instance;
+    WindowClass.hCursor = LoadCursor(0, IDC_ARROW);
+    //WindowClass.hIcon = ;
+    WindowClass.lpszClassName = "DiGaMaSoWindowClass";
+
+    
+    if (RegisterClassA(&WindowClass))
+    {
+        HWND Window = CreateWindowExA(0, WindowClass.lpszClassName, "DiGaMaSo", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, 0, 0, instance, 0);
+        
+        if (Window) 
+        {
+            
+        }
+        
+    }
+        
+
     return 0;
 }
